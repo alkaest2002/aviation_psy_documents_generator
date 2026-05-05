dfb2e674ce96d63e6f415e5de62b433aaa68fc9d
Pierpaolo Calanna <p.calanna@gmail.com>
2026-05-05 13:01:30 +0200

    Update

--- lib/data_invite_speaker.py
+++ lib/data_invite_speaker.py
@@ -17,14 +17,16 @@ def get_data(options: str | None = None) -> dict[str, Any]:
     all_speakers = []
     processed_speakers = []
 
+    paperDates = data.get("paperDates", {})
+
     # Extract all talks and speakers from the program data
-    for day in data["days"]:
-        for event in day["events"]:
+    for day in data.get("days", []):
+        for event in day.get("events", []):
             if event.get("eventType") == "talk":
-                all_talks.append({**event, "date": day["date"]})
+                all_talks.append({**event, "date": day.get("date"), "panel": event.get("title")})
             if event.get("eventType") == "panel":
                 for talk in event.get("talks", []):
-                    all_talks.append({**talk, "date": day["date"]})
+                    all_talks.append({**talk, "date": day.get("date"), "panel": event.get("title")})
 
     # Process each talk to extract speaker information, 
     # ensuring that speakers with the name "Autore da definire" are excluded.
@@ -33,11 +35,14 @@ def get_data(options: str | None = None) -> dict[str, Any]:
         for author in authors:
             if not author.get("name") == "Autore da definire":
                 all_speakers.append({
-                    "date": talk["date"],
-                    "timeWindow": talk["timeWindow"],
-                    **author, 
-                    "title": talk.get("title"), 
-                    "co-authors": [ a for a in authors if a != author ]
+                    "talk_date": talk.get("date"),
+                    "talk_timeWindow": talk.get("timeWindow"),
+                    "talk_title": talk.get("title"), 
+                    "talk_panel": talk.get("panel"),
+                    "talk_duration": talk.get("duration"),
+                    **{f"author_{k}": v for k, v in author.items()}, 
+                    "author_collaborate_with": [ a for a in authors if a != author ],
+                    **paperDates
                 })
 
     # In this context, options is expected to be a jq filter 
@@ -54,7 +59,7 @@ def get_data(options: str | None = None) -> dict[str, Any]:
     # Process each speaker and prepare data for rendering
     for speaker in speakers:
         processed_speakers.append([
-            f"{re.sub(r'[^\w]', '_', speaker['name'].lower()).strip('_')}", 
+            f"{re.sub(r'[^\w]', '_', speaker['author_name'].lower()).strip('_')}", 
             speaker
         ])
         
